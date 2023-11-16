d1005fc947df52f79ee960232a752ea0ee3c34a0
noahdavis05 <146333718+noahdavis05@users.noreply.github.com>
2023-11-16 10:50:04 +0000

    This is my work

--- task2/FitnessDataStruct.h
+++ task2/FitnessDataStruct.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #ifndef FITNESS_DATA_STRUCT_H
 #define FITNESS_DATA_STRUCT_H
@@ -22,5 +23,61 @@ FILE *open_file(char filename[], char mode[]) {
     return file;
 }
 
+//function to find how many items are in file
+int count_items(char filename[], char mode[]){
+    FILE *file = open_file(filename, mode);
+
+    //read file and work out how many records there are in the file
+    int count = 0;
+    int buffer_size = 100;
+    char line_buffer[buffer_size];
+    while (fgets(line_buffer,buffer_size,file) != NULL){
+        count++;
+    }
+    //close file so it can be opened again from the start
+    fclose(file);
+    return count;
+}
+
+//function to find date with fewest steps
+int earliest_date(char filename[], char mode[]){
+	int count;
+	count = count_items(filename,"r");
+	FILE *file = open_file(filename, mode);
+	FITNESS_DATA my_data[count];
+	//add data to this array
+	char date[11];
+	char time[6];
+	char steps[10];
+	//make a counter variable 
+	int counter = 0;
+	int buffer_size = 100;
+	char line_buffer[buffer_size];
+	//adding data
+	while (fgets(line_buffer,buffer_size,file) != NULL){
+		tokeniseRecord(line_buffer,",",date,time,steps);
+		strcpy(my_data[counter].date, date);
+		strcpy(my_data[counter].time,time);
+		my_data[counter].steps = atoi(steps);
+		counter++;
+	}
+
+	int lowest_steps = my_data[0].steps;
+	char lowest_date[15];
+	char lowest_time[15];
+
+	//iterate through data
+	for (int i = 0; i < count; i++){
+		if (my_data[i].steps <= lowest_steps){
+			lowest_steps = my_data[i].steps;
+			strcpy(lowest_date,my_data[i].date);
+			strcpy(lowest_time,my_data[i].time);
+		}
+	}
+	        
+	printf("%s %s\n",lowest_date, lowest_time);
+	return 0;
+
+}
 #endif // FITNESS_DATA_STRUCT_H
 
--- task2/StepCounter_Final.c
+++ task2/StepCounter_Final.c
@@ -44,14 +44,27 @@ void tokeniseRecord(const char *input, const char *delimiter,
 int main() {
     // Declare variables
     int cont = 0;
+    char my_option[100];
     char option;
     char filename[100];
+    int i;
+    int count;
+    char low_date[15];
+    int fewest_steps;
     // make a while loop to contain the switch case statement so that it can be done multiple times
     while (cont == 0){
         // print out options for user to choose
         printf("Menu Options:\nA: Specify the filename to be imported\nB: Display the total number of records in the file\nC: Find the date and time of the timeslot with the fewest steps\nD: Find the date and time of the timeslot with the largest number of steps\nE: Find the mean step count of all the records in the file\nF: Find the longest continuous period where the step count is above 500 steps\nQ: Quit\nEnter choice: ");
-        option = getchar();
-        while (getchar() != '\n');
+        scanf("%s",my_option);
+        for (i = 0; my_option[i] != '\0'; ++i); //finds the length of the string to make sure a input was just one character long
+        //check length
+        if (i == 1){
+            option = my_option[0];
+        } else {
+            option = 'z'; //this will make the switch case default
+        }
+
+        
         // switch case statement
         switch (option){
             case 'A':
@@ -61,20 +74,25 @@ int main() {
                 getchar(); // Consume the newline character
                 FILE *my_file = open_file(filename,"r");
                 if (my_file == NULL){
-                    printf("Unsuccesful \n");
+                    printf("Error: Could not find or open the file.\n");
+                    exit(1);
                 } else {
-                    printf("Success \n");
+                    fclose(my_file);
                 }
                 break;
 
+
             case 'B':
             case 'b':
-                printf("b");
+            // specify the number of lines in the file
+                count = count_items(filename,"r");
+                printf("%d\n",count);
                 break;
 
             case 'C':
             case 'c':
-                printf("c");
+                //create a structured array
+                count = count_items(filename,"r");
                 break;
 
             case 'D':
@@ -100,7 +118,7 @@ int main() {
 
         // if they type anything else:
         default:
-            printf("Invalid choice\n");
+            printf("Invalid choice. Try again.\n");
             break;
         }
     }
